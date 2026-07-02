928d2d59a72d96a9c879b2894eddb762648c56a7
coope <cbjhockey56@gmail.com>
2026-07-02 16:00:51 -0500

    feat: add /privacy route

---
+++ app/privacy/page.tsx
@@ -0,0 +1,10 @@
+import PrivacySection from "../components/PrivacySection";
+
+export const metadata = {
+  title: "Privacy Policy - Cooper Mapes",
+  description: "Privacy Policy for coopermapes.com.",
+};
+
+export default function PrivacyPage() {
+  return <PrivacySection />;
+}
